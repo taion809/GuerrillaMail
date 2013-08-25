30309224fa17aa1ec4a1fe0b28a31a745afe682f
Nicholas J <nicholas.a.johns5@gmail.com>
2013-08-26 08:16:38 +0900

    Bugfix issue #10

--- src/Johnsn/GuerrillaMail/GuerrillaConnect/CurlConnection.php
+++ src/Johnsn/GuerrillaMail/GuerrillaConnect/CurlConnection.php
@@ -87,8 +87,6 @@ class CurlConnection extends Connection
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
 
-        var_dump(http_build_query($query));
-
         $response = json_decode($output, true);
 
         $data = array();
